614d42208f51c5fb166daba0d0ed51e9562438c8
kentomiyazawa <kento4241@yahoo.co.jp>
2020-06-17 21:32:29 +0900

    ストップとリセットボタン

--- StopWatchApp/ViewController.swift
+++ StopWatchApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
     
     var startTime: TimeInterval? = nil
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +25,16 @@ class ViewController: UIViewController {
 //        print(Date.timeIntervalSinceReferenceDate)
         if let startTime = self.startTime {
             let t: Double = Date.timeIntervalSinceReferenceDate - startTime
-            print(t)
+            let min = Int(t / 60)
+            let sec = Int(t) % 60
+            let msec = Int((t - Double(sec)) * 100.0)
+            self.timerLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
         }
     }
     @IBAction func startTimer(_ sender: Any) {
         // スタートボタンを押した時の時間
         self.startTime = Date.timeIntervalSinceReferenceDate
-        Timer.scheduledTimer(
+        self.timer = Timer.scheduledTimer(
             timeInterval: 0.01,
             target: self,
             selector: #selector(self.update),
@@ -38,8 +42,12 @@ class ViewController: UIViewController {
             repeats: true)
     }
     @IBAction func stopTimer(_ sender: Any) {
+//        invalidate()メソッドはタイマーを止める
+        self.timer.invalidate()
     }
     @IBAction func resetTimer(_ sender: Any) {
-    }
+        self.startTime = nil
+        self.timerLabel.text = "00:00:00"
 }
 
+}
