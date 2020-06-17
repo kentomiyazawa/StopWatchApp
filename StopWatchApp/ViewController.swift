c095cb54a54667ab904f437a4396b04b71240b2b
kentomiyazawa <kento4241@yahoo.co.jp>
2020-06-17 21:15:56 +0900

    スタートボタンを作成

--- StopWatchApp/ViewController.swift
+++ StopWatchApp/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var startTime: TimeInterval? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @objc func update() {
+        // 2001/1/1 00:00:00が基準値
+//        print(Date.timeIntervalSinceReferenceDate)
+        if let startTime = self.startTime {
+            let t: Double = Date.timeIntervalSinceReferenceDate - startTime
+            print(t)
+        }
+    }
+    @IBAction func startTimer(_ sender: Any) {
+        // スタートボタンを押した時の時間
+        self.startTime = Date.timeIntervalSinceReferenceDate
+        Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(self.update),
+            userInfo: nil,
+            repeats: true)
+    }
+    @IBAction func stopTimer(_ sender: Any) {
+    }
+    @IBAction func resetTimer(_ sender: Any) {
+    }
 }
 
